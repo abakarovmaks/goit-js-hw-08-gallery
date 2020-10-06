ba8313a5bf1cccf6a1116da4d839aeb8fe180bc9
abakarovmaks <abakarovmaks@gmail.com>
2020-10-06 22:50:57 +0300

    Делает ветку li->a->img и закидывает её в ul

--- photos.js
+++ photos.js
@@ -1,9 +1,24 @@
 import photos from "./gallary-items.js";
 console.log(photos);
 
-const gallaryEl = document.createElement("li");
-gallaryEl.classList.add("gallery__item");
+const makeGalleryCards = function ({ original, preview, description }) {
+  const mainUl = document.querySelector("js-gallery");
 
-const linkEl = document.createElement("a");
-linkEl.href = photos.preview;
-linkEl.classList.add("gallery__link");
+  const gallaryEl = document.createElement("li");
+  gallaryEl.classList.add("gallery__item");
+
+  const linkEl = document.createElement("a");
+  linkEl.href = original;
+  linkEl.classList.add("gallery__link");
+
+  const imageEl = document.createElement("img");
+  imageEl.classList.add("gallery__image");
+  imageEl.src = preview;
+  imageEl.dataset = original;
+  imageEl.alt = description;
+
+  mainUl.append(gallaryEl, linkEl, imageEl);
+  return mainUl;
+};
+
+console.log(makeGalleryCards[0]);
